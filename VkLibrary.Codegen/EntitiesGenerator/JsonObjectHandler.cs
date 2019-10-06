eeddc2fe09a4051963faca87f1e0fe00bdccf856
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-10-06 17:11:30 +0300

    chore: add sample project for parsing vk json scheme

---
+++ VkLibrary.Codegen/EntitiesGenerator/JsonObjectHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace VkLibrary.Codegen.EntitiesGenerator
+{
+    public class JsonObjectHandler
+    {
+        public static void CheckType(JObject schemeObject)
+        {
+            if (!schemeObject.ContainsKey("type"))
+                throw new ArgumentException($"Object don't contains type: {schemeObject}");
+
+            var type = schemeObject["type"].Value<string>();
+            switch (type)
+            {
+                case "object":
+                    break;
+                case "enum":
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid type: {type}");
+            }
+        }
+    }
+}
