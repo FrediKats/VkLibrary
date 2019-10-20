8074f43f61625a34217ed28019a683b88ada626f
Fredi Kats <fredikats@outlook.com>
2019-10-20 14:49:18 +0300

    fix: correct parsing for responses with inner type

---
+++ VkLibrary.Codegen/Tools/TypeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace VkLibrary.Codegen.Tools
+{
+    public class TypeProvider
+    {
+        public static readonly TypeProvider Instance = new TypeProvider();
+
+        public Dictionary<string, string> Mapper { get; } = new Dictionary<string, string>();
+
+        public void Register(string alias, string originalType)
+        {
+            Mapper[alias] = originalType;
+        }
+
+        public string Resolve(string alias)
+        {
+            if (!Mapper.ContainsKey(alias))
+                return alias;
+
+            string type = Mapper[alias];
+            Log.Instance.Message($"Replace [{alias}] with [{type}]");
+            return type;
+        }
+    }
+}
